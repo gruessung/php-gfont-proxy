f48af30f2f105011238eab4efc2358e586936162
Alexander Grüßung <info@gruessung.eu>
2022-12-27 21:26:37 +0100

    replace file_get_contents with curl

--- index.php
+++ index.php
@@ -38,8 +38,18 @@ function cacheFile($url, $filepath) {
     $url = base64_decode($url);
     $url = str_replace('http://', 'https://', $url);
     if (strpos($url, 'fonts.googleapis.com') !== false || strpos($url, 'fonts.gstatic.com') !== false) {
-        $content = file_get_contents($url);
-        $content = str_replace('https://', $_ENV['BASE_URL'].'/?url=https://', $content);
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch);
+        }
+        curl_close($ch);
+        $content = str_replace('https://', $_ENV['BASE_URL'].'/?url=https://', $result);
+        
         file_put_contents($filepath, $content);
     } else {
         die('invalid: '.$url);
